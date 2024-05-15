25f562df96c1432e727f02ec5132734254c52258
MeishaPD <putradewanm@student.ub.ac.id>
2024-05-15 17:01:59 +0700

    Setelah Konversi Fragment

---
+++ app/src/main/java/com/example/projectfitlyp4/ui/LunchFragment.java
@@ -0,0 +1,74 @@
+package com.example.projectfitlyp4.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.projectfitlyp4.FoodAdapter;
+import com.example.projectfitlyp4.R;
+import com.example.projectfitlyp4.database.Food;
+import com.example.projectfitlyp4.database.FoodAPI;
+import com.example.projectfitlyp4.database.FoodApiService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class LunchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<Food> lunchList = new ArrayList<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+        recyclerView = view.findViewById(R.id.rvBreakfast);
+
+        // Set up RecyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        FoodAdapter adapter = new FoodAdapter(getActivity(), lunchList);
+        recyclerView.setAdapter(adapter);
+
+        // Load breakfast data
+        FoodApiService api = FoodAPI.getClient().create(FoodApiService.class);
+        Call<List<Food>> lunch = api.getLunch();
+        lunch.enqueue(new Callback<List<Food>>() {
+            @Override
+            public void onResponse(Call<List<Food>> call, Response<List<Food>> response) {
+                if (response.isSuccessful()) {
+                    lunchList = response.body();
+                    if (lunchList != null) {
+                        FoodAdapter lunchAdapter = new FoodAdapter(getActivity(), lunchList);
+                        recyclerView.setAdapter(lunchAdapter);
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "Failed to fetch food data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Food>> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(getActivity(), "Failed to fetch food data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
